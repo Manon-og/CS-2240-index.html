fed30e9b7cb561c44011768f38dacf2dea24dbbf
James A. Manon-og <115532696+Manon-og@users.noreply.github.com>
2024-02-17 21:54:00 +0800

    Add files via upload

---
+++ mint.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'b') {
+        lenk();
+    }
+});
+
+function lenk() {
+    location.href = "https://shopee.ph/list/doublemint";
+}
